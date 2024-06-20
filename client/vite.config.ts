3937dc157b861c4321aa80fea12b170045ebe4b6
Repayment-inc <repayment.inc@gmail.com>
2024-06-20 12:33:33 +0900

    【enhance - client】 vercel.json追加 preview環境でrobots.txtを読み込む

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+const isPreview = process.env.NODE_ENV === "preview";
+console.log("環境変数 = " + process.env.NODE_ENV);
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
@@ -14,4 +17,11 @@ export default defineConfig({
   build: {
     outDir: "./dist",
   },
+  ...(isPreview ? { assetsInclude: ["robots.txt"] } : {}), // preview環境をクローリング対象外にするため
+
+  /**
+   * 今後GTMも実装し、環境によって出し分ける予定
+   * https://sunday-morning.app/posts/2021-05-02-vercel-preview-fixed-subdomain
+   * https://qiita.com/AsilHatake/items/905a5bccddca584dc068
+   */
 });
